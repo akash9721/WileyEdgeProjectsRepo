21898ac07cd43c65511ad27c033875b5d6650f06
Akashkumar972177 <akashkumar972177@gmail.com>
2022-09-13 09:18:17 +0530

    Day5 commit

--- Day3/src/com/java/assignment/ValidateDate.java
+++ Day3/src/com/java/assignment/ValidateDate.java
@@ -2,7 +2,7 @@ package com.java.assignment;
 
 public class ValidateDate {
 	public static void main(String[] args) {
-		Date d = new Date(30, 10, 2022);
+		Date d = new Date(30, 12, 2022);
 		if (d.checkDate())
 			System.out.println("Date is Valid");
 		else {
