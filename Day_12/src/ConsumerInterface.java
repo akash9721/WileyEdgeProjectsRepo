1814c27c43badd679ae01f0aa6d9fd5b86c9c537
Akashkumar972177 <akashkumar972177@gmail.com>
2022-09-26 00:09:02 +0530

    commit

--- Day_12/src/ConsumerInterface.java
+++ Day_12/src/ConsumerInterface.java
@@ -3,7 +3,8 @@ import java.util.List;
 
 public class ConsumerInterface {
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(2, 4, 5, 6, 7, 9);
+
+		List<Integer> list = Arrays.asList(2, 4, 5, 6, 7, 9, 10);
 		list.forEach(n -> System.out.println(n)); // for each take obj of consumer interface
 	}
 
